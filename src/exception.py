759d2160e8a951e0126883bd1efc790deb03315e
Adityagnss <adityagnss@gmail.com>
2024-07-24 20:27:31 +0530

    exception and logger

---
+++ src/exception.py
@@ -0,0 +1,34 @@
+"""
+
+python define exception
+To define an exception in Python, you can create a new class that inherits from the Exception 
+class or a built-in exception class. Here's a simple example:
+
+class CustomError(Exception):
+    pass
+
+This CustomError class is a basic custom exception. You can add attributes or methods to it as 
+needed to provide more information about the error.
+
+"""
+
+import sys
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
---
+++ src/logger.py
@@ -0,0 +1,55 @@
+'''
+
+Python machine learning (ML) projects, we use logger files for several reasons:
+
+• Auditing and debugging: 
+
+Log files provide a record of important events in the application, allowing developers to identify 
+and troubleshoot issues. By capturing detailed information about errors, warnings, and informative messages, 
+we can reproduce and fix problems more efficiently.
+
+• Compliance and regulatory requirements:
+
+In industries like finance, healthcare, or government, logging is often mandated by 
+regulations (e.g., HIPAA, GDPR) to maintain transparency and accountability.
+
+• Monitoring and performance tracking: 
+
+Log files can contain metrics and statistics about model performance, training, and 
+inference, enabling us to monitor the system's behavior and make data-driven decisions.
+
+In Python, we use the built-in logging module, which provides a flexible and customizable way to handle logging. The basic components of a logger setup include:
+
+• Logger: The central instance that emits log messages. 
+
+• Handler: Responsible for sending log records to a specific location 
+(e.g., file, console, network). • Formatter: Formats and styles the log records, controlling the output format and content.
+
+
+'''
+
+
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m+__%d_%Y_%H_%M_%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
+
+
+if __name__ == "__main__":
+    logging.info("logging has been started")
